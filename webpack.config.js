217461bb34007d89d6aaf3756c039b4ed91a93b2
Yung <neiklotrr@gmail.com>
2022-11-16 17:15:06 +0300

    Boilerplate and dev env set up.

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports ={
+    mode: 'development',
+    entry: path.resolve(__dirname, 'src/index.js'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        clean: true,
+        assetModuleFilename: '[name][ext]'
+    },
+    devtool: 'source-map',
+    module: {
+        rules:[
+            {
+                test:/\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif|)$/i,
+                type: 'asset/resource',
+                generator:{
+                    filename: 'assets/[name][ext]'
+                }
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Restaurant',
+            filename: 'index.html',
+            template: 'src/template.html',
+        })
+    ]
+}
